81a0ad32111f8ec2eac48e0ef3f958bf38a2e7e6
Melina Pashna <melinaapashna@gmail.com>
2024-08-18 17:21:16 -0700

    DailyDetails update

--- src/components/DailyDetail/DailyDetail.tsx
+++ src/components/DailyDetail/DailyDetail.tsx
@@ -5,11 +5,13 @@ const DailyDetail = ({ weekDay, icon, temp_l, temp_h }: IDailyDetail) => {
   const store = mainStore();
   const dayOfWeek = store.getWeekday(weekDay);
   return (
-    <div className="text-sm w-1/3 bg-blue-600 flex flex-col justify-around items-center h-full">
-      <span className="">{dayOfWeek}</span>
-      <img src={icon} alt="icon_condition" />
-      <span>{temp_h}&#176;</span>
-      <span>{temp_l}&#176;</span>
+    <div className="text-sm w-1/3 bg-blue-600/70 backdrop-blur-md flex flex-col justify-around items-center h-full p-4 rounded-lg shadow-md text-white">
+      <span className="font-semibold">{dayOfWeek}</span>
+      <img src={icon} alt="icon_condition" className="w-12 h-12 my-2" />
+      <div className="flex justify-between w-full px-2">
+        <span className="font-medium">{temp_h}&#176;</span>
+        <span className="font-light">{temp_l}&#176;</span>
+      </div>
     </div>
   );
 };
