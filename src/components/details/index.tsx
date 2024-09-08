15d873aba1957c0cb573d6c60e7c65a4c80d8501
Melina Pashna <melinaapashna@gmail.com>
2024-09-08 14:00:10 -0700

    Details component more responsive

--- src/components/details/index.tsx
+++ src/components/details/index.tsx
@@ -13,18 +13,19 @@ const Details = () => {
   );
 
   return (
-    <div className="flex flex-col items-center mt-5 w-full max-w-4xl mx-auto">
+    <div className="flex flex-col items-center mt-5 w-full max-w-4xl mx-auto p-4">
       <div className="relative w-full">
         <SearchCityName />
       </div>
+      
       <motion.ul
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1, delay: 1.9 }}
         className="flex flex-col gap-y-2 mt-4 w-full"
       >
-        <h4 className="font-bold text-xl text-white text-center">Details</h4>
-        <div className="flex justify-around text-sm text-white bg-blue-00/70 backdrop-blur-md p-4 rounded-lg shadow-md">
+        <h4 className="font-bold text-lg md:text-xl lg:text-2xl text-white text-center">Details</h4>
+        <div className="flex flex-col sm:flex-row justify-around text-xs md:text-sm lg:text-base text-white bg-blue-00/70 backdrop-blur-md p-2 md:p-4 rounded-lg shadow-md">
           <li className="flex flex-col justify-center items-center">
             <span className="font-medium">Feels Like:</span>
             <span>{store.weatherInfo?.feelslike}&#176;</span>
@@ -43,18 +44,19 @@ const Details = () => {
           </li>
         </div>
       </motion.ul>
+      
       <motion.ul
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1, delay: 1.9 }}
         className="flex flex-col gap-y-4 w-full mt-4"
       >
-        <div className="w-full m-auto tablet:max-w-[22rem]">
+        <div className="w-full sm:w-[22rem] md:w-[28rem] lg:w-[32rem] m-auto">
           <ButtonGroup>
             <button
               onClick={(e) => store.getIdElement(e)}
               id="hour"
-              className={`text-sm px-4 py-2 rounded-lg hover:bg-gray-500 hover:text-white transition-colors duration-300 ${
+              className={`text-sm px-2 py-1 md:px-4 md:py-2 rounded-lg hover:bg-gray-500 hover:text-white transition-colors duration-300 ${
                 store.currentEleID === "hour"
                   ? "text-white bg-blue-600"
                   : "text-blue-600 bg-white"
@@ -65,7 +67,7 @@ const Details = () => {
             <button
               id="daily"
               onClick={(e) => store.getIdElement(e)}
-              className={`text-sm px-4 py-2 rounded-lg hover:bg-blue-500 hover:text-white transition-colors duration-300 ${
+              className={`text-sm px-2 py-1 md:px-4 md:py-2 rounded-lg hover:bg-blue-500 hover:text-white transition-colors duration-300 ${
                 store.currentEleID === "daily"
                   ? "text-white bg-blue-600"
                   : "text-blue-600 bg-white"
@@ -74,24 +76,25 @@ const Details = () => {
               Daily
             </button>
           </ButtonGroup>
-          <div className="info w-full h-[10rem] border bg-white/10 backdrop-blur-md rounded-lg shadow-md overflow-hidden mt-4">
+          
+          <div className="info w-full h-[8rem] md:h-[10rem] border bg-white/10 backdrop-blur-md rounded-lg shadow-md overflow-hidden mt-4">
             {store.currentEleID === "hour" && (
-              <div className="w-full flex h-full gap-2 p-2">
+              <div className="w-full flex h-full gap-2 p-2 overflow-x-auto">
                 {forcastHour.map((item, index) => {
                   return (
                     <HourlyDetail
-                    key={index}
-                    time={item.time.slice(11, 13)}
-                    icon={item.icon}
-                    temp_c={item.temp_c}
-                    humidity={item.humidity}
+                      key={index}
+                      time={item.time.slice(11, 13)}
+                      icon={item.icon}
+                      temp_c={item.temp_c}
+                      humidity={item.humidity}
                     />
                   );
                 })}
               </div>
             )}
             {store.currentEleID === "daily" && (
-              <div className="h-full flex gap-2 p-2">
+              <div className="h-full flex gap-2 p-2 overflow-x-auto">
                 {store.daysData.map((item, idx) => {
                   return (
                     <DailyDetail
