8ed749ca1ad21fb5eb392944b1713ad59abf4d13
Melina Pashna <melinaapashna@gmail.com>
2024-09-08 14:07:13 -0700

    Search more responsive

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -6,7 +6,7 @@ const SearchCityName = () => {
 
   return (
     <form 
-      className="relative flex items-center w-full max-w-md mx-auto"
+      className="relative flex items-center w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto"
       onSubmit={(e) => { store.onSubmitSearchBtn(e) }}
     >
       <input
@@ -14,15 +14,15 @@ const SearchCityName = () => {
         value={store.inputCityLocation}
         onChange={(e) => { store.onChange(e) }}
         type="text"
-        className="w-full bg-blue-600/10 text-white px-6 py-2 border-none outline-none placeholder-slate-100 rounded-l-lg backdrop-blur-md"
+        className="w-full bg-blue-600/10 text-white px-4 py-2 sm:px-6 sm:py-2 md:px-8 md:py-3 lg:px-10 lg:py-4 border-none outline-none placeholder-slate-100 rounded-l-lg backdrop-blur-md text-sm md:text-base"
         placeholder="City Name"
       />
       <button 
         type="submit" 
-        className="bg-blue-600/10 p-2 rounded-r-lg hover:bg-blue-700 transition-colors duration-300 backdrop-blur-md"
+        className="bg-blue-600/10 p-2 sm:p-2 md:p-3 lg:p-4 rounded-r-lg hover:bg-blue-700 transition-colors duration-300 backdrop-blur-md"
       >
         <CiSearch 
-          className="w-6 h-6 text-white"
+          className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 text-white"
         />
       </button>
     </form>
