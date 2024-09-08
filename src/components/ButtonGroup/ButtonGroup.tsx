8ecd34aa413b479b6271c94411d5a6ed43df0c9d
Melina Pashna <melinaapashna@gmail.com>
2024-09-08 13:55:35 -0700

    button responsive

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -2,7 +2,7 @@ import IBtnGroup from "./type";
 
 const ButtonGroup = ({ children }: IBtnGroup) => {
   return (
-    <div className="flex gap-2 p-4 bg-blue-00/80 backdrop-blur-sm rounded-lg shadow-lg">
+    <div className="flex gap-2 md:gap-4 p-2 md:p-4 lg:p-6 bg-blue-500/80 backdrop-blur-sm rounded-lg shadow-lg">
       {children}
     </div>
   );
